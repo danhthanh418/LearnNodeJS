0e3779e3189e55bbc12432edafe123b927ff4731
Thanh Danh <15520798@gm.uit.edu.vn>
2018-11-21 14:18:01 +0700

    add circle file

---
+++ Shape/circle.js
@@ -0,0 +1,7 @@
+const {PI} = Math //const PI =Math.PI
+// const PI2 = Math.PI
+module.exports={
+    area: (radius)=>{
+        return PI*radius*radius
+    } 
+}
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // Import file rectangle
 const rect = require('./Shape/rectangle')
-
+let circle = require('./Shape/circle')
 console.log(`The area of the reactangle (10,20) is ${rect.area(10,20)}`)
 console.log(`The cir of the reactangle (10,20) is ${rect.cir(10, 20)}`)
+console.log(`Date is ${rect.curentDateTime}`)
 console.log(`Dir name ${rect.directoryName}`)
 console.log(`File name ${rect.fileName}`)
+console.log(`Area of circle have radius = 10 is ${circle.area(10)}`)
