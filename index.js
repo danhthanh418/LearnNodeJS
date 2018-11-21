38f7473fdba082e730fdeda8e86dff54b2a04e9f
Thanh Danh <15520798@gm.uit.edu.vn>
2018-11-21 13:56:56 +0700

    change file reactangle to reactangle

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Import file rectangle
-const rect = require('./Shape/reactangle')
+const rect = require('./Shape/rectangle')
 
 console.log(`The area of the reactangle (10,20) is ${rect.area(10,20)}`)
 console.log(`The cir of the reactangle (10,20) is ${rect.cir(10, 20)}`)
